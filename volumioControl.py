8cd1df63d9e8de93b6224ae86ba65792a3a9fe79
Aurélien Martinez <aurelien.mrt@gmail.com>
2019-08-05 19:35:33 +0200

    ajout du rec

--- volumioControl.py
+++ volumioControl.py
@@ -7,16 +7,21 @@ ser = serial.Serial('/dev/ttyACM0', 9600)
 vol = Volumio()
 mifare = nxppy.Mifare()
 
-while True:
-  commande = ser.readline().decode().strip()
-  if commande == 'Play':
-      vol.play_song()
-  print(commande) 
+def nfc_read_card():
   try:
+    time.sleep(1)
     uid = mifare.select()
     print(uid)
+    return uid
   except nxppy.SelectError:
         # SelectError is raised if no card is in the field.
     pass
 
-  time.sleep(1)
+while True:
+  if ser.inWaiting() > 0:
+    commande = ser.readline().decode().strip()
+    if commande == 'Rec':
+      playlist = nfc_read_card()
+      print(commande)
+      print(playlist) 
+       
