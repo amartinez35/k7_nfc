166eddcf318e48be5e9c529b57e5fd5eea4f9096
Aurélien Martinez <aurelien.mrt@gmail.com>
2019-08-05 19:13:20 +0200

    lecture nfc

--- volumioControl.py
+++ volumioControl.py
@@ -1,11 +1,22 @@
 import serial
+import time
+import nxppy
 from Volumio import Volumio
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 vol = Volumio()
+mifare = nxppy.Mifare()
 
 while True:
   commande = ser.readline().decode().strip()
   if commande == 'Play':
       vol.play_song()
   print(commande) 
+  try:
+    uid = mifare.select()
+    print(uid)
+  except nxppy.SelectError:
+        # SelectError is raised if no card is in the field.
+    pass
+
+  time.sleep(1)
