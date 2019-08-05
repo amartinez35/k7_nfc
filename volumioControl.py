289df393030823bd76b4d18e746245610eadf47f
Aurélien Martinez <aurelien.mrt@gmail.com>
2019-08-05 12:53:25 +0200

    test de la lecture

--- volumioControl.py
+++ volumioControl.py
@@ -131,8 +131,12 @@ while True:
         GPIO.cleanup()
         pass
 import serial
+from Volumio import Volumio
 ser = serial.Serial('/dev/ttyACM0', 9600)
+vol = Volumio()
 
 while True:
   commande = ser.readline().decode().strip()
+  if commande == 'Play':
+      vol.play_song()
   print(commande) 
